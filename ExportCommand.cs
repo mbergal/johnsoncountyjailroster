c9fca85f337f3995f740467b1fc441f6a61e9248
Misha Bergal <misha.bergal@gmail.com>
2013-10-13 22:29:22 -0500

    Export to .csv

---
+++ ExportCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using ManyConsole;
+
+namespace JailRoster
+    {
+    class ExportCommand : ConsoleCommand
+        {
+        public ExportCommand()
+            {
+            IsCommand( "export" );
+            }
+
+        public override int Run(string[] remainingArguments)
+            {
+            using ( var dataContext  = new JailRosterDataContext()) 
+                {
+                var records = from d in dataContext.Detainees
+                                from o in d.Offenses
+                                    select new ExcelExportRecord 
+                                        {
+                                        ReportDate = d.ReportDate,
+                                        DetaineeName = d.Name,
+                                        Age = d.Age,
+                                        DateBooked = d.DateBooked,
+                                        HousingFacility = d.HousingFacility,
+                                        JailRosterId = d.JailRosterId,
+                                        OffenseLevel = o.OffenseLevel,
+                                        OffenseName = o.OffenseName,
+                                        CauseNumber = o.CauseNumber,
+                                        CodeSection = o.CodeSection,
+                                        BondType = o.BondType,
+                                        BondAmount = o.BondAmount,
+                                        ChargeReleased = o.ChargeReleased
+                                        };
+
+                using ( var textWriter = new StreamWriter( "export.csv" ) )
+                using ( var csvWriter = new CsvHelper.CsvWriter( textWriter ) )
+                    csvWriter.WriteRecords( records );
+                }
+            return 0;
+            }
+        }
+
+    internal class ExcelExportRecord 
+        {
+        public DateTime ReportDate { get; set; }
+        public int      JailRosterId { get; set; }
+        public string   DetaineeName { get; set; }
+        public int      Age { get; set; }
+        public DateTime DateBooked { get; set; }
+        public string   HousingFacility { get; set; }
+        public string   OffenseLevel { get; set; }
+        public string   OffenseName { get; set; }
+        public string   CauseNumber { get; set; }
+        public string   CodeSection { get; set; }
+        public string   BondType { get; set; }
+        public string   BondAmount { get; set; }
+        public string   ChargeReleased { get; set; }
+        }
+    }
---
+++ ScrapeCommand.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+using ManyConsole;
+
+namespace JailRoster
+    {
+    internal class ScrapeCommand : ConsoleCommand
+        {
+        public ScrapeCommand()
+            {
+            IsCommand( "scrape" );
+            }
+
+        public override int Run( string[] remainingArguments )
+            {
+            using ( var dataContext  = new JailRosterDataContext()) 
+                {
+                var index = ScrapeIndex();
+                foreach ( var detailUrl in index )
+                try {
+                    var detainee = ScrapeDetainee("https://ww1.johnson-county.com/" + detailUrl );
+                    detainee.ReportDate = DateTime.Today;
+                    dataContext.Detainees.Add( detainee );
+                    }
+                catch ( Exception ex ) // Jail Roster MVC frontend is buggy
+                    {
+                    Console.WriteLine( ex.Message + ex.StackTrace );
+                    }
+                dataContext.SaveChanges();
+                }
+            return 0;
+            }
+
+        static private string[]   ScrapeIndex()
+            {
+            var index = new WebClient().DownloadString( "https://ww1.johnson-county.com/Sheriff/JailRoster/FullList/?filter=RosterType%3Aall%7CName%3A" );
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml( index );
+            var trs = doc.DocumentNode.SelectNodes("//table[@class='data']/tr[string-length(@id) > 0]").ToArray();
+            var details = trs.Select( x=> x.SelectSingleNode( "td/span[@id='details']/input").GetAttributeValue( "value", "" )).ToArray();
+            return details.ToArray();
+            }
+
+        static private Detainee   ScrapeDetainee( string url )
+            {
+            var details = new WebClient().DownloadString( url );
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml( details );
+            var detainee = new Detainee()
+                {
+                    JailRosterId = Int32.Parse( doc.DocumentNode.SelectSingleNode( "//input[@id='jID']").GetAttributeValue( "value", "" ) ),
+                    Name = doc.DocumentNode.SelectSingleNode( "//input[@id='n']").GetAttributeValue( "value", "" ),
+                    Age = Int32.Parse( doc.DocumentNode.SelectSingleNode( ".//table[@class='data']/tr/td").InnerText ),
+                    DateBooked = DateTime.Parse( doc.DocumentNode.SelectSingleNode( ".//table[@class='data']/tr/td[2]").InnerText  ),
+                    HousingFacility = doc.DocumentNode.SelectSingleNode( ".//table[@class='data']/tr/td[3]").InnerText.Trim( "\n\r ".ToCharArray()),
+                    Offenses = ScrapeOffenses( doc )
+                };
+            
+
+            return detainee;
+            }
+
+        private static Offense[] ScrapeOffenses(HtmlDocument doc)
+            {
+            var offenses = doc.DocumentNode.SelectNodes( "//div[@id='offense']" );
+            return offenses != null 
+                       ? offenses.Select( ScrapeOffense ).ToArray() 
+                       : new Offense[] {};
+            }
+
+        private static Offense ScrapeOffense(HtmlNode htmlNode)
+            {
+            return new Offense()
+                {
+                    OffenseName = htmlNode.SelectSingleNode( ".//table/tr[2]/td[1]").InnerText.TrimWhitespace(),
+                    CodeSection = htmlNode.SelectSingleNode( ".//table/tr[2]/td[2]").InnerText.TrimWhitespace(),
+                    OffenseLevel = htmlNode.SelectSingleNode( ".//table/tr[2]/td[3]").InnerText.TrimWhitespace(),
+                    CauseNumber =  htmlNode.SelectSingleNode( ".//table[2]/tr[2]/td[1]").InnerText.TrimWhitespace(),
+                    BondType = htmlNode.SelectSingleNode( ".//table[2]/tr[2]/td[2]").InnerText.TrimWhitespace(),
+                    BondAmount = htmlNode.SelectSingleNode( ".//table[2]/tr[2]/td[3]").InnerText.TrimWhitespace(),
+                    ChargeReleased = htmlNode.SelectSingleNode( ".//table[2]/tr[2]/td[4]").InnerText.TrimWhitespace(),
+
+                };
+            }
+
+        }
+    }
